60f6a33f3bb8fd4e72fb0e52b8bf4a04fb5809cb
marcelasra <marcelasrandrade@gmail.com>
2024-05-09 22:21:57 -0300

    exercicios de fixaçao

--- src/campeonato/AppCampeonatoVarzea.java
+++ src/campeonato/AppCampeonatoVarzea.java
@@ -1,23 +1,40 @@
 package campeonato;
 
+import java.util.Scanner;
+
 public class AppCampeonatoVarzea {
 
     public static void main(String[] args) {
-        Time t[] = new Time[4];
+        Scanner sc = new Scanner(System.in);
+        Time time = new Time();
+        System.out.println("BEM VINDO AO MENU \n");
+        System.out.println("O que vocÊ deseja fazer?\n[1] - cadastrar times\n[2] - ver status do time\n[3] - ver apresentação de um time\n[4] - ver a execução um jogo\n[5] - sair");
+        int op = sc.nextInt();
+        switch (op) {
 
-        t[0] = new Time("Santa Peba", "Sao Paulo", "SP", 1998, 0, 0, 0, 11);
-        t[1] = new Time("Cumbitas", "Osasco", "SP", 2001, 0, 0, 0, 12);
-        t[2] = new Time("Belisco", "Itú", "SP", 2003, 0, 0, 0, 8);
-        t[3] = new Time("Bazucas", "Cunha", "SP", 2010, 0, 0, 0, 10);
+            case 1:
+               
+                System.out.println("Quantos times você deseja cadastrar? ");
+                int timesCadastro = sc.nextInt();
+                Time t[] = new Time[timesCadastro];
+                time.CadastrarTime(t);
 
-        Jogo jg = new Jogo();
-
-        jg.marcarJogo(t[0], t[3]);
-        jg.jogar(); 
-
-        for (Time time : t) {
-            time.status();
+            case 2:
+                System.out.println("Qual status vc deseja buscar? ");
+                
+                time.CadastrarTime(t);
         }
+
+        //t[0] = new Time("Santa Peba", "Sao Paulo", "SP", 1998, 0, 0, 0, 11);
+        //t[1] = new Time("Cumbitas", "Osasco", "SP", 2001, 0, 0, 0, 12);
+        //t[2] = new Time("Belisco", "Itú", "SP", 2003, 0, 0, 0, 8);
+        //t[3] = new Time("Bazucas", "Cunha", "SP", 2010, 0, 0, 0, 10);
+        //Jogo jg = new Jogo();
+        //jg.marcarJogo(t[0], t[3]);
+        //jg.jogar();
+        //for (Time time : t) {
+        //    time.status();
+        //}
     }
 
 }
--- src/campeonato/Time.java
+++ src/campeonato/Time.java
@@ -1,6 +1,9 @@
 package campeonato;
 
+import java.util.Scanner;
+
 public class Time {
+
     private String nome;
     private String cidade;
     private String uf;
@@ -9,10 +12,14 @@ public class Time {
     private int derrotas;
     private int empates;
     private int qtedeJogadores;
-    
-    public Time(String nome, String cidade, 
-            String uf, int anoFundacao, int vi, 
-            int de, int em, int qtedeJogadores){
+
+    public Time() {
+
+    }
+
+    public Time(String nome, String cidade,
+            String uf, int anoFundacao, int vi,
+            int de, int em, int qtedeJogadores) {
         this.nome = nome;
         this.cidade = cidade;
         this.uf = uf;
@@ -23,43 +30,79 @@ public class Time {
         this.qtedeJogadores = qtedeJogadores;
     }
 
-public String apresentar() {
-String dados = "";
-dados += "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
-dados += "\nSALVE AMIGOS do CAMPEONATO VARZEANO!\n";
-dados += "O grande time " + this.getNome() + "\n";
-dados += "Diretamente de " + this.getCidade() + "," + this.getUf() + "\n";
-dados += "Fundado no ano de " + this.getAnoFundacao() + "\n";
-dados += "Vitórias: " + this.getVitorias() + "\n";
-dados += "Derrotas: " + this.getDerrotas() + "\n";
-dados += "Empates: " + this.getEmpates() + "\n";
-dados += "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
-return dados;
+    public String apresentar() {
+        String dados = "";
+        dados += "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
+        dados += "\nSALVE AMIGOS do CAMPEONATO VARZEANO!\n";
+        dados += "O grande time " + this.getNome() + "\n";
+        dados += "Diretamente de " + this.getCidade() + "," + this.getUf() + "\n";
+        dados += "Fundado no ano de " + this.getAnoFundacao() + "\n";
+        dados += "Vitórias: " + this.getVitorias() + "\n";
+        dados += "Derrotas: " + this.getDerrotas() + "\n";
+        dados += "Empates: " + this.getEmpates() + "\n";
+        dados += "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n";
+        return dados;
 
-}
+    }
 
-public String status() {
-String dados = "";
-dados += "\n************************************************\n";
-dados += "Time: " + this.getNome() + "\n";
-dados += "Vitórias: " + this.getVitorias() + "\n";
-dados += "Derrotas: " + this.getDerrotas() + "\n";
-dados += "Empates: " + this.getEmpates() + "\n";
-dados += "\n************************************************\n";
-return dados;
+    public String status() {
+        String dados = "";
+        dados += "\n************************************************\n";
+        dados += "Time: " + this.getNome() + "\n";
+        dados += "Vitórias: " + this.getVitorias() + "\n";
+        dados += "Derrotas: " + this.getDerrotas() + "\n";
+        dados += "Empates: " + this.getEmpates() + "\n";
+        dados += "\n************************************************\n";
+        return dados;
 
-}
-public void ganharJogo(){
-    this.setVitorias(this.getVitorias() + 1);
-}
+    }
 
-public void perderJogo(){
-    this.setDerrotas(this.getDerrotas() + 1);
-}
-public void empatarJogo(){
-    this.setEmpates(this.getEmpates() + 1);
-}
+    public void CadastrarTime(Time t[]) {
+       
+        Scanner txt = new Scanner(System.in);
+        Scanner num = new Scanner(System.in);
+        int i = 0;
+        do{
 
+            t[i] = new Time();
+            System.out.println("Informe o nome do time: ");
+            t[i].setNome(txt.nextLine());
+
+            System.out.println("Informe a cidade do time: ");
+            t[i].setCidade(txt.nextLine());
+
+            System.out.println("Informe o estado do time: ");
+            t[i].setUf(txt.nextLine());
+
+            System.out.println("Informe o ano de fundação do time: ");
+            t[i].setAnoFundacao(num.nextInt());
+
+            System.out.println("Informe as vitorias do time: ");
+            t[i].setVitorias(num.nextInt());
+
+            System.out.println("Informe as derrotas do time: ");
+            t[i].setDerrotas(num.nextInt());
+
+            System.out.println("Informe os empates do time: ");
+            t[i].setEmpates(num.nextInt());
+
+            System.out.println("Informe a quantidade de jogadores do time: ");
+            t[i].setQtedeJogadores(num.nextInt());
+            
+        }while(i < t.length);
+    }
+
+    public void ganharJogo() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderJogo() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarJogo() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
 
     public String getNome() {
         return nome;
